dad8bfa8bae258f7f7d2f825879cdf2aabed9988
Lucas Costa <ls.coosta@hotmail.com>
2025-04-24 12:46:28 -0300

    ✨ feat: project structure

---
+++ src/test/java/com/lucascostabr/vaga_certa/StartupTests.java
@@ -0,0 +1,13 @@
+package com.lucascostabr.vaga_certa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
